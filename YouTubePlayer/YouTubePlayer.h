38c76508dfaa645a6bea0f853f6ee9b7ede07859
Akio Yasui <akkyie01@gmail.com>
2017-08-22 09:31:47 +0900

    Initial Commit

---
+++ YouTubePlayer/YouTubePlayer.h
@@ -0,0 +1,19 @@
+//
+//  YouTubePlayer.h
+//  YouTubePlayer
+//
+//  Created by Akio Yasui on 2017/08/22.
+//  Copyright © 2017 Akio Yasui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YouTubePlayer.
+FOUNDATION_EXPORT double YouTubePlayerVersionNumber;
+
+//! Project version string for YouTubePlayer.
+FOUNDATION_EXPORT const unsigned char YouTubePlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouTubePlayer/PublicHeader.h>
+
+
